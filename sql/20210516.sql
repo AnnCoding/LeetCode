3dbe3d0ed010c3792b1461243c8fb5c34e2a6276
AnnCoding <18292683306@163.com>
2021-05-16 23:06:24 +0800

    sql2

--- sql/20210516.sql
+++ sql/20210516.sql
@@ -10,3 +10,25 @@ select distinct salary as SecondHighestSalary from Employee order by salary desc
 -- 修正能够输出null的
 select IFNULL(
 (select distinct(Salary)from Employee order by Salary desclimit 1,1),null) as SecondHighestSalary;
+
+-- 获取employee 表中第N高的薪水
+CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
+BEGIN
+  SET N = N-1;
+  IF N < 0 THEN
+  RETURN NULL;
+  ELSE
+  RETURN (
+      # Write your MySQL query statement below.
+      SELECT IFNULL(
+          (
+              SELECT
+              DISTINCT Salary
+              FROM Employee
+              ORDER BY Salary desc
+              LIMIT N,1
+          ),NULL
+      ) AS getNthHighestSalary
+  );
+  END IF;
+END
