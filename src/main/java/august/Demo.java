7e9688c83de5ba0afe868f22046cb2a3bda4a2ba
AnnCoding <18292683306@163.com>
2023-01-02 23:27:47 +0800

    2

--- src/main/java/august/Demo.java
+++ src/main/java/august/Demo.java
@@ -1,5 +1,10 @@
 package august;
 
+import com.sun.tools.javac.util.StringUtils;
+
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * @author chenjiena
  * @version 1.0
@@ -8,5 +13,23 @@ package august;
 public class Demo {
 
     public static void main(String[] args) {
+        String astr = "helo";
+        System.out.println(isUnique(astr));
     }
+
+    public static boolean isUnique(String astr) {
+        if (astr == null){
+            return false;
+        }
+        char[] c = astr.toCharArray();
+        Set<Character> ss = new HashSet<>();
+        for (int i = 0;i<c.length;i++){
+            if (!ss.add(c[i])){
+                return false;
+            }
+        }
+
+        return true;
+    }
+
 }
