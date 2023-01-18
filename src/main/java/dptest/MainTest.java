fa3a6bb588eaa06a869c61d2cf3b7e89d5dd59fc
AnnCoding <18292683306@163.com>
2023-01-18 17:24:28 +0800

    补充上一周的题

--- src/main/java/dptest/MainTest.java
+++ src/main/java/dptest/MainTest.java
@@ -1,5 +1,8 @@
 package dptest;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class MainTest {
 
     public static void main(String[] args) {
@@ -78,13 +81,54 @@ public class MainTest {
 
         for (int i = 1; i < m; i++) {
             for (int j = 1; j < n; j++) {
-                grid[i][j] += Math.max(grid[i][j-1],grid[i-1][j]);
+                grid[i][j] += Math.max(grid[i][j - 1], grid[i - 1][j]);
             }
         }
-        return grid[m-1][n-1];
+        return grid[m - 1][n - 1];
     }
 
+    //把数字翻译成字符串 - 动态规划 ： 最后一个能翻译  和  最后两个能翻译的 两种方式//todo 没太懂
+    public int translateNum(int num) {
+        String src = String.valueOf(num);
+        int n = src.length();
 
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+        dp[1] = 1;
+        for (int i = 2; i <= n; ++i) {
+            dp[i] += dp[i - 1];
+            String pre = src.substring(i - 2, i);
+            if (pre.compareTo("10") >= 0 && pre.compareTo("25") <= 0) {
+                dp[i] += dp[i - 2];
+            }
+        }
+        return dp[n];
+    }
+
+    //最长不含重复的字符的子字符串
+    public int lengthOfLongestSubstring(String s) {
+        //哈希集合，记录每个字符串出现过
+        Set<Character> occ = new HashSet<>();
+        int n = s.length();
+        int rk = -1,ans = 0;
+        //右指针，初始化为-1 相当于我们在字符串的左边界的左侧，还没有开始移动
+        for (int i = 0;i<n;++i){
+            if (i != 0){
+                //左指针向右移动一格，移除一个字符串
+                occ.remove(s.charAt(i-1));
+            }
+
+            while (rk+1 < n && !occ.contains(s.charAt(rk+1))){
+                //不断地移动右指针
+                occ.add(s.charAt(rk + 1));
+                ++rk;
+            }
+            //第i 到 rk 个字符是一个极长的无重复字符子串
+            ans = Math.max(ans,rk-i+1);
+        }
+
+        return ans;
+    }
 
 
 }
