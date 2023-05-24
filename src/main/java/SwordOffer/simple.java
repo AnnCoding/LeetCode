0a1b8e1fd5945b321cd53d84fd09de82fa696803
AnnCoding <18292683306@163.com>
2023-05-24 22:03:11 +0800

    补充1天的

--- src/main/java/SwordOffer/simple.java
+++ src/main/java/SwordOffer/simple.java
@@ -2,6 +2,7 @@ package SwordOffer;
 
 import java.util.Arrays;
 import java.util.HashSet;
+import java.util.PriorityQueue;
 import java.util.Set;
 
 /**
@@ -18,6 +19,62 @@ public class simple {
 
         System.out.println(divide(a, b));
     }
+
+    /**
+     * 从这里开始----------------
+     * @return
+     */
+
+    //最小的k个数
+    public int[] getLeastNumbers(int[] arr, int k) {
+        int[] vec = new int[k];
+        Arrays.sort(arr);
+        for (int i = 0; i < k; ++i) {
+            vec[i] = arr[i];
+        }
+        return vec;
+    }
+
+
+    PriorityQueue<Integer> queMin;
+    PriorityQueue<Integer> queMax;
+
+    public simple() {
+        queMin = new PriorityQueue<Integer>((a, b) -> (b - a));
+        queMax = new PriorityQueue<Integer>((a, b) -> (a - b));
+    }
+
+    public void addNum(int num) {
+        if (queMin.isEmpty() || num <= queMin.peek()) {
+            queMin.offer(num);
+            if (queMax.size() + 1 < queMin.size()) {
+                queMax.offer(queMin.poll());
+            }
+        } else {
+            queMax.offer(num);
+            if (queMax.size() > queMin.size()) {
+                queMin.offer(queMax.poll());
+            }
+        }
+    }
+
+    public double findMedian() {
+        if (queMin.size() > queMax.size()) {
+            return queMin.peek();
+        }
+        return (queMin.peek() + queMax.peek()) / 2.0;
+    }
+
+
+
+
+
+
+    /**
+     * -------------------从这里结束
+     * @param nums
+     * @return
+     */
 
     //把数组排成最小的数
     public String minNumber(int[] nums) {
