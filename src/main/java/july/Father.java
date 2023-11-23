0a36d371f029941d0c4ec5f6cbd8bc2554edd3b5
AnnCoding <18292683306@163.com>
2023-11-23 19:34:00 +0800

    提交一波

--- src/main/java/july/Father.java
+++ src/main/java/july/Father.java
@@ -36,3 +36,30 @@ public class Father {
     }
 
 }
+
+
+class Singleton {
+    // 使用 volatile 关键字确保多线程下的可见性
+    private static volatile Singleton instance;
+
+    // 私有构造方法，防止外部直接实例化
+    private Singleton() {
+    }
+
+    // 获取单例实例的方法
+    public static Singleton getInstance() {
+        // 第一次检查
+        if (instance == null) {
+            // 加锁，保证只有一个线程可以进入创建实例的代码块
+            synchronized (Singleton.class) {
+                // 第二次检查，因为可能在第一个线程获得锁后，另一个线程已经创建了实例
+                if (instance == null) {
+                    instance = new Singleton();
+                }
+            }
+        }
+        return instance;
+    }
+
+    // 其他业务方法
+}
--- src/main/java/july/learn.java
+++ src/main/java/july/learn.java
@@ -1,10 +1,39 @@
 package july;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class learn {
 
     public static void main(String[] args) {
         int n = 0;
-        System.out.println(fib(n));
+//        System.out.println(fib(n));
+
+
+        int i = 9,j=9;
+
+//        System.out.println(j == i++); true
+//        System.out.println(i == i); true
+//        System.out.println(j == ++i); false
+//        System.out.println(++j == i); false
+//        System.out.println(j++ == i); true
+
+
+        ArrayList<String> a = new ArrayList<>();
+        a.add("a");
+        a.add("b");
+
+        String[] str = new String[]{"a","b"};
+        List<String> b = Arrays.asList(str);
+
+//        ArrayList<String> c = (ArrayList<String>) a.subList(0,1); //报错了
+//        String[] d = (String[]) a.toArray(); //报错
+
+        b.remove(0);//报错
+
+
     }
 
     public static int fib(int n){
